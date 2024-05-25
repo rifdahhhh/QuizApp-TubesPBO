1ecae67636a7129d660643d94e2358b5509aeae9
krisnasantosa <krisnasantosa@smknegeri1garut.sch.id>
2024-05-25 10:25:03 +0700

    add database, libraries .jar

--- db_quizapp.sql
+++ db_quizapp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.1deb1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 24, 2024 at 03:14 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: May 25, 2024 at 03:22 AM
+-- Server version: 8.0.34
+-- PHP Version: 8.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int(8) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL
+  `id` int NOT NULL,
+  `username` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -47,13 +47,13 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 --
 
 CREATE TABLE `questions` (
-  `id` varchar(10) NOT NULL,
-  `name` varchar(500) NOT NULL,
-  `opt1` varchar(500) NOT NULL,
-  `opt2` varchar(500) NOT NULL,
-  `opt3` varchar(500) NOT NULL,
-  `opt4` varchar(500) NOT NULL,
-  `answer` varchar(500) NOT NULL
+  `id` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `opt1` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `opt2` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `opt3` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `opt4` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `answer` varchar(500) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -61,7 +61,9 @@ CREATE TABLE `questions` (
 --
 
 INSERT INTO `questions` (`id`, `name`, `opt1`, `opt2`, `opt3`, `opt4`, `answer`) VALUES
-('1', 'percobaan', 'a', 'b', 'c', 'd', 'insert');
+('1', 'percobaan', 'halo', 'b', 'c', 'd', 'insert'),
+('2', 'nama binatang kecuali', 'bayam', 'beruang', 'singa', 'burung', 'bayam'),
+('3', 'Pertanyaan, kecuali', 'apa', 'siapa', 'dimana', 'lihat', 'lihat');
 
 --
 -- Indexes for dumped tables
@@ -81,7 +83,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
